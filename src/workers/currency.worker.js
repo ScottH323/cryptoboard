6cfd3b68722175cf108dd6ae039440fb9a762a30
Scott <s.hague@managedracks.co.uk>
2018-01-31 19:49:35 +0000

    fix path issue on worker

--- src/workers/currency.worker.js
+++ src/workers/currency.worker.js
@@ -16,7 +16,7 @@ async function getCurrency() {
     console.log('Checking currency index');
 
     // const res = await axios.get('http://localhost:3000/currency/table');
-    const res = await axios.get(`${config.BASE_URL}/currency/table`);
+    const res = await axios.get(`${config.BASE_URL}currency/table`);
 
     if (res.status === 200)
         self.postMessage(res.data);
